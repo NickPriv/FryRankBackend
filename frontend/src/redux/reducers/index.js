4e9ffbaccd0ccb51c822b8d5bdeecf2f93780d5f
oxyflush <dan.zhan10@gmail.com>
2024-02-05 20:23:36 -0800

    Remove frontend directory
    This commit deletes the frontend directory since this repo is being
    converted to host only the backend.
    
    Additionally, change the backend application to allow calls from the
    frontend herok URL and modify procfile to only build the backend now.

--- frontend/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import restaurantsReducer from './restaurants';
-import reviewsReducer from './reviews';
-
-export default combineReducers({
-    reviewsReducer,
-    restaurantsReducer,
-});
