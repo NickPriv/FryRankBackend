c8886c1ad31d5e91a6355d11f42b1e1e4056889b
Nicholas Privitera <nicholaspriv@gmail.com>
2024-07-08 22:24:11 -0700

    Adding accountId as a field of the Review object

--- src/main/java/com/fryrank/validator/ReviewValidator.java
+++ src/main/java/com/fryrank/validator/ReviewValidator.java
@@ -10,7 +10,8 @@ import java.time.format.DateTimeParseException;
 
 public class ReviewValidator implements Validator {
     public static final String ISO_DATE_TIME = "isoDateTime";
-    public static final String ISO_DATE_TIME_REJECTION_REQUIRED_CODE = "field.required";
+    public static final String ACCOUNT_ID = "accountId";
+    public static final String REJECTION_REQUIRED_CODE = "field.required";
     public static final String ISO_DATE_TIME_REJECTION_FORMAT_CODE = "field.invalidFormat";
     public static final String ISO_DATE_TIME_REJECTION_FORMAT_REASON = "The provided isoDateTime is not in ISO format.";
     @Override
@@ -23,7 +24,7 @@ public class ReviewValidator implements Validator {
         Review review = (Review) target;
         String isoDateTime = review.getIsoDateTime();
         if(isoDateTime == null) {
-            errors.rejectValue(ISO_DATE_TIME, ISO_DATE_TIME_REJECTION_REQUIRED_CODE);
+            errors.rejectValue(ISO_DATE_TIME, REJECTION_REQUIRED_CODE);
         }
         else {
             try {
@@ -32,6 +33,12 @@ public class ReviewValidator implements Validator {
             catch(DateTimeParseException e) {
                 errors.rejectValue(ISO_DATE_TIME, ISO_DATE_TIME_REJECTION_FORMAT_CODE, ISO_DATE_TIME_REJECTION_FORMAT_REASON);
             }
+        }
+
+        // Validate account ID
+        final String accountId = review.getAccountId();
+        if (accountId == null) {
+            errors.rejectValue(ACCOUNT_ID, REJECTION_REQUIRED_CODE);
         }
     }
 
