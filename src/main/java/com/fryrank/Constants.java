75ecda1881435f576d218a17599c516a4ab05d6e
oxyflush <dan.zhan10@gmail.com>
2025-01-13 21:43:52 -0800

    Fix indenting

--- src/main/java/com/fryrank/Constants.java
+++ src/main/java/com/fryrank/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     // Input Validator
     public static final String GENERIC_VALIDATOR_ERROR_MESSAGE = "Encountered error while validating API input.";
     public static final String REVIEW_VALIDATOR_ERRORS_OBJECT_NAME = "review";
-public static final String USER_METADATA_VALIDATOR_ERRORS_OBJECT_NAME = "userMetadata";
+    public static final String USER_METADATA_VALIDATOR_ERRORS_OBJECT_NAME = "userMetadata";
     public static final String REJECTION_REQUIRED_CODE = "field.required";
     public static final String REJECTION_FORMAT_CODE = "field.invalidFormat";
 
