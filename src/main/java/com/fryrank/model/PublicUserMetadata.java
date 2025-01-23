4b9aabe4cb6ca2290a028883997a8288ce9581ac
Daniel Zhang <dan.zhan10@gmail.com>
2025-01-22 20:49:06 -0800

    Fix issue with usernames being returned as null
    This commit fixes an issue with the user metadata API returning null
    when given valid user IDs. This is because the collection name
    annotation was renamed to public-user-metadata when the collection name
    didn't change.

--- src/main/java/com/fryrank/model/PublicUserMetadata.java
+++ src/main/java/com/fryrank/model/PublicUserMetadata.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("public-user-metadata")
+@Document("user-metadata")
 @Data
 public class PublicUserMetadata {
     @Id
